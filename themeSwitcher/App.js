8475958ba2c2823de75323fd3ea53a7b8686cd4d
nilekant93 <nikolas.hirvonen@gmail.com>
2024-02-26 22:06:53 +0200

    ThemeSwitcher

---
+++ themeSwitcher/App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PaperProvider } from 'react-native-paper';
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+import MainAppbar from './components/MainAppbar';
+import { NavigationContainer } from '@react-navigation/native';
+import ThemeProvider from './context/ThemeProvider';
+import { ThemeContext } from './context/ThemeContext';
+
+export default function App() {
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <PaperProvider>
+      <ThemeProvider>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#666" barStyle="light-content"/>
+          <Stack.Navigator
+            initialRouteName='Home'
+            screenOptions={{header: (props) =>
+              <MainAppbar {...props} backgroundColor="#666" icon="cog" color="#fff"/>
+            }}
+          >
+            <Stack.Screen name="Home">
+              {() =>
+                <Home />
+              }
+            </Stack.Screen>
+            <Stack.Screen name="Settings" component={Settings}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 3,
+    backgroundColor: '#fff',
+  },
+});
